d4034d96c30a5e281d6fa10fd951d4490ad06d98
maxim <karachyov@musan.kz>
2018-05-22 17:29:37 +0600

    about

--- src/js/profootball.js
+++ src/js/profootball.js
@@ -5,5 +5,14 @@
 
 $(function () {
     $('input[type=checkbox],input[type=file], select').styler();
+    $('.popap_box').click(function() {
+        $('#modalbox_' + $(this).data('body')).arcticmodal({
+            overlay: {
+                css: {
+                    backgroundColor: '#000'
+                }
+            }
+        });
+    });
 });
 
