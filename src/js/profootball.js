fd43c51cbdd78ae74ffba079bdae2ae96024267e
maxim <karachyov@musan.kz>
2018-05-23 12:57:21 +0600

    menu scroll

--- src/js/profootball.js
+++ src/js/profootball.js
@@ -14,5 +14,11 @@ $(function () {
             }
         });
     });
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top-50}, 1500);
+    });
 });
 
